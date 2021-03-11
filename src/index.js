ce1763e0eb9b2296ed11bbf1ee2a0ec307444348
notReally08 <dasha.pavlovna2014@yandex.ru>
2021-03-11 13:33:31 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let arr = n.toString().split("");
+  arr.reverse();
+  while(arr[0] === 0) {
+    arr.shift();
+  }
+  let str = arr.join("");
+  return Number.parseInt(str)
 }
